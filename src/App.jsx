58d8400692c44226d76426ec6ba8ede369460b36
Fadi-Ayman <Fady.dev8@gmail.com>
2024-07-10 11:32:14 +0300

    Make Code Splitting (Lazy Loading) For Page Routing, For More Performance Optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,58 @@
 import { CityProvider } from "./contexts/CityContext";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import Pricing from "./pages/Pricing";
-import Product from "./pages/Product";
-import Login from "./pages/Login";
-import AppLayout from "./pages/AppLayout";
-import PageNotFound from "./pages/PageNotFound";
+import { AuthProvider } from "./contexts/AuthContext";
+import { lazy, Suspense } from "react";
+
+// import Homepage from "./pages/Homepage";
+// import Pricing from "./pages/Pricing";
+// import Product from "./pages/Product";
+// import Login from "./pages/Login";
+// import AppLayout from "./pages/AppLayout";
+// import PageNotFound from "./pages/PageNotFound";
+
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { AuthProvider } from "./contexts/AuthContext";
+import SpinnerFullPage from './components/SpinnerFullPage';
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Product = lazy(() => import("./pages/Product"));
+const Login = lazy(() => import("./pages/Login"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+
 
 function App() {
   return (
     <AuthProvider>
       <CityProvider>
-
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="applayout" element={<AppLayout />}>
-              <Route
-                index
-                element={
-                  <Navigate to={"cities"} replace>
-                    <CityList />
-                  </Navigate>
-                }
-              />
-              <Route path="cities" element={<CityList />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="city/:id" element={<City />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+          <Suspense fallback={<SpinnerFullPage/>} >
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/pricing" element={<Pricing />} />
+              <Route path="/product" element={<Product />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="applayout" element={<AppLayout />}>
+                <Route
+                  index
+                  element={
+                    <Navigate to={"cities"} replace>
+                      <CityList />
+                    </Navigate>
+                  }
+                />
+                <Route path="cities" element={<CityList />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="city/:id" element={<City />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
-        
       </CityProvider>
     </AuthProvider>
   );
